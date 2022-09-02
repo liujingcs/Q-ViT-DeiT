dc9527a9879286bbb17ffb8c527deadc1f81c58f
liujingcs <liujing_95@outlook.com>
2022-09-02 20:56:15 +1000

    Update script.

--- script/m3_b/uniform_train_wn_cifar_w4a4.sh
+++ script/m3_b/uniform_train_wn_cifar_w4a4.sh
@@ -4,6 +4,8 @@ lr=2e-4
 epochs=100
 id=4bit_uniform
 
+for j in 1 2 3 4
+do
 python -m torch.distributed.launch \
 --nproc_per_node=2 --use_env main.py \
 --model deit_wn_tiny_patch16_224_mix \
@@ -15,7 +17,9 @@ python -m torch.distributed.launch \
 --wbits ${wbits} \
 --abits ${abits} \
 --dist-eval \
---output_dir results/deit_wn_tiny_${id}_cifar/${wbits}w${abits}a_bs512_baselr${lr}_ft${epochs} \
+--output_dir results/deit_wn_tiny_${id}_cifar/${wbits}w${abits}a_bs512_baselr${lr}_ft${epochs}_seed${j} \
 --finetune /scratch/dl65/bzhuang/jing/Q-ViT-DeiT/results/deit_tiny_float_cifar/ckpt/best_checkpoint.pth \
 --data-set CIFAR \
---data-path /home/bzhuang/dl65/bzhuang/cifar100
+--data-path /home/bzhuang/dl65/bzhuang/cifar100 \
+--seed ${j}
+done
