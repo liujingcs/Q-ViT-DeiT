1306c785fb7cf8a84f49436db448dba53347856b
liujingcs <liujing_95@outlook.com>
2022-09-03 17:47:14 +1000

    Add script to train qlsam.

--- script/m3/uniform_train_qlsam_wn_cifar_w2a2.sh
+++ script/m3/uniform_train_qlsam_wn_cifar_w2a2.sh
@@ -4,7 +4,7 @@ lr=2e-4
 epochs=100
 id=2bit_uniform
 
-for rho in 0.2 0.3 0.4
+for rho in 0.05
 do
 for j in 0
 do
@@ -25,6 +25,7 @@ python -m torch.distributed.launch \
 --data-path /home/jliu/dl65/liujing/dataset/cifar \
 --seed ${j} \
 --rho ${rho} \
---sam_type "QLSAM"
+--sam_type "QLSAM" \
+--resume /scratch/dl65/jing/Codes/Q-ViT-DeiT/results/deit_wn_tiny_2bit_uniform_cifar/2w2a_bs512_baselr2e-4_ft100_QLSAM_rho0.05_0/ckpt/current_checkpoint.pth
 done
 done
