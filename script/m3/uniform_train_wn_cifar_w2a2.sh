9edc9e9375e3bb4ca76203d65d13c89056409a79
liujingcs <liujing_95@outlook.com>
2022-09-05 15:12:42 +1000

    Update training script.

--- script/m3/uniform_train_wn_cifar_w2a2.sh
+++ script/m3/uniform_train_wn_cifar_w2a2.sh
@@ -4,12 +4,12 @@ lr=2e-4
 epochs=100
 id=2bit_uniform
 
-for j in 1 2 3 4
+for j in 0
 do
 python -m torch.distributed.launch \
 --nproc_per_node=2 --use_env main.py \
 --model deit_wn_tiny_patch16_224_mix \
---batch-size 64 \
+--batch-size 128 \
 --lr ${lr} \
 --min-lr 0 \
 --epochs ${epochs} \
@@ -17,8 +17,8 @@ python -m torch.distributed.launch \
 --wbits ${wbits} \
 --abits ${abits} \
 --dist-eval \
---output_dir results/deit_wn_tiny_${id}_cifar/${wbits}w${abits}a_bs512_baselr${lr}_ft${epochs}_${j} \
---finetune /scratch/dl65/jing/Codes/Q-ViT-DeiT/results/deit_tiny_float_cifar/ckpt/best_checkpoint.pth \
+--output_dir results/deit_wn_tiny_${id}_cifar_bs128/${wbits}w${abits}a_bs512_baselr${lr}_ft${epochs}_${j} \
+--finetune /scratch/dl65/jing/Codes/Q-ViT-DeiT/results/deit_tiny_float_cifar_bs128/ckpt/best_checkpoint.pth \
 --data-set CIFAR \
 --data-path /home/jliu/dl65/liujing/dataset/cifar \
 --seed ${j}
