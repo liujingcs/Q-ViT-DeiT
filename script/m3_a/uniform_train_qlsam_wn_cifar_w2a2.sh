248636585f63a06854973531c715fc1a8db27776
liujingcs <liujing_95@outlook.com>
2022-09-05 15:54:09 +1000

    Update training script.

---
+++ script/m3_a/uniform_train_qlsam_wn_cifar_w2a2.sh
@@ -0,0 +1,30 @@
+wbits=2
+abits=2
+lr=2e-4
+epochs=100
+id=2bit_uniform
+
+for rho in 0.02 0.03 0.05
+do
+for j in 0
+do
+python -m torch.distributed.launch \
+--nproc_per_node=2 --use_env main_sam.py \
+--model deit_wn_tiny_patch16_224_mix \
+--batch-size 128 \
+--lr ${lr} \
+--min-lr 0 \
+--epochs ${epochs} \
+--warmup-epochs 0 \
+--wbits ${wbits} \
+--abits ${abits} \
+--dist-eval \
+--output_dir results/deit_wn_tiny_${id}_cifar_bs128/${wbits}w${abits}a_bs512_baselr${lr}_ft${epochs}_QLSAM_rho${rho}_${j} \
+--finetune /scratch/dl65/pzz/jing/Codes/iclr2022/Q-ViT-DeiT/results/deit_tiny_float_cifar_bs128/ckpt/best_checkpoint.pth \
+--data-set CIFAR \
+--data-path /home/jliu/dl65/liujing/dataset/cifar \
+--seed ${j} \
+--rho ${rho} \
+--sam_type "QLSAM"
+done
+done
