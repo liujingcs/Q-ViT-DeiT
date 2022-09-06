ab1f0913655dd1c933c1a55312e714e7935d62b4
liujingcs <liujing_95@outlook.com>
2022-09-07 01:42:36 +1000

    Update script.

--- script/m3_z/uniform_train_qlsamv2_wn_cifar_w2a2.sh
+++ script/m3_z/uniform_train_qlsamv2_wn_cifar_w2a2.sh
@@ -8,8 +8,8 @@ for rho in 0.01 0.007 0.005 0.003 0.001
 do
 for j in 0
 do
-CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch \
---master_port 62255 \
+CUDA_VISIBLE_DEVICES=2,3 python -m torch.distributed.launch \
+--master_port 62251 \
 --nproc_per_node=2 --use_env main_sam.py \
 --model deit_wn_tiny_patch16_224_mix \
 --batch-size 128 \
