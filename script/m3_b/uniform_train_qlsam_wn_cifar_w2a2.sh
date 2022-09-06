e49621f51f708a3c6c25b95a73e675baebe1c0f4
liujingcs <liujing_95@outlook.com>
2022-09-06 19:32:23 +1000

    Update script.

---
+++ script/m3_b/uniform_train_qlsam_wn_cifar_w2a2.sh
@@ -0,0 +1,30 @@
+wbits=2
+abits=2
+lr=2e-4
+epochs=100
+id=2bit_uniform
+
+for rho in 0.005 0.007 0.003 0.001
+do
+for j in 0
+do
+python -m torch.distributed.launch \
+--nproc_per_node=2 --use_env main_sam.py \
+--model deit_wn_tiny_patch16_224_mix \
+--batch-size 128 \
+--lr ${lr} \
+--min-lr 0 \
+--epochs ${epochs} \
+--warmup-epochs 0 \
+--wbits ${wbits} \
+--abits ${abits} \
+--dist-eval \
+--output_dir results/deit_wn_tiny_${id}_cifar_bs128/${wbits}w${abits}a_bs512_baselr${lr}_ft${epochs}_QLSAM_rho${rho}_${j} \
+--finetune /scratch/dl65/bzhuang/jing/Q-ViT-DeiT/results/deit_tiny_float_cifar_bs128/ckpt/best_checkpoint.pth \
+--data-set CIFAR \
+--data-path /home/bzhuang/dl65/bzhuang/cifar100 \
+--seed ${j} \
+--rho ${rho} \
+--sam_type "QLSAM"
+done
+done
