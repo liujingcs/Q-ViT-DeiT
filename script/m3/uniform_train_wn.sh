b9cf3d6cf013a05d471d557e2ef17455ce7132d8
liujingcs <liujing_95@outlook.com>
2022-09-07 10:04:11 +1000

    Update script.

---
+++ script/m3/uniform_train_wn.sh
@@ -0,0 +1,20 @@
+wbits=4
+abits=4
+lr=5e-4
+epochs=300
+id=4bit_uniform
+
+python -m torch.distributed.launch \
+--nproc_per_node=8 --use_env main.py \
+--model deit_wn_tiny_patch16_224_mix \
+--batch-size 64 \
+--lr ${lr} \
+--min-lr 0 \
+--epochs ${epochs} \
+--warmup-epochs 0 \
+--wbits ${wbits} \
+--abits ${abits} \
+--dist-eval \
+--output_dir results/deit_wn_tiny_${id}/${wbits}w${abits}a_bs512_baselr${lr}_ft${epochs} \
+--finetune /scratch/dl65/jing/Codes/Q-ViT-DeiT/results/deit_tiny_float/ckpt/best_checkpoint.pth \
+--data-path /home/jliu/dl65/m3_imagenet
